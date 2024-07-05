4d9dbbb5b19af0129c673759ebba8b80c48cca6e
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-07-05 00:10:44 +0000

    triangle

--- more_functions_nested_loops/10-main.c
+++ more_functions_nested_loops/10-main.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
 * main - check the code
@@ -7,9 +7,9 @@ include "main.h"
 */
 int main(void)
 {
-print_triangle(2);
-print_triangle(10);
-print_triangle(1);
-print_triangle(0);
-return (0);
+	print_triangle(2);
+	print_triangle(10);
+	print_triangle(1);
+	print_triangle(0);
+	return (0);
 }
--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,16 +1,34 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_triangle - prints a triangle, followed by a new line.
- * @size - number of times '#' should be printed
+ * @size: number of '#' and spaces should be printed
  *
  * If size is 0 or less, the function should print only a new line.
  *
  */
-void print_square(int size)
+void print_triangle(int size)
 {
-	int i, j;
+	int rowno, space, symbol;
 
-	if (size > 0)
-	{	
-		for (i = 1; i <= size; i++)
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (rowno = 1; rowno <= size; rowno++)
+		{
+			for (space = 1; space <= (size - rowno); space++)
+			{
+				_putchar(32);
+			}
+			for (symbol = 1; symbol <= rowno; symbol++)
+			{
+				_putchar(35);
+			}
 
+			_putchar('\n');
+		}
+	}
+}
