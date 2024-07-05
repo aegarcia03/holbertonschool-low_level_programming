174786c9c5c7aa3c1b8f21c0dbe75d271c0c2623
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-07-05 10:49:36 +0000

    length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string.
+ * @s: string to be evaluated.
+ *
+ * Return: the length of the string.
+ *
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+
+	}
+	return (length);
+}
