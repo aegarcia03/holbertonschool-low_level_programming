c4fc25549256b8be9a4c6153e2a787b94782f4fb
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-07-06 04:53:00 +0000

    pointer

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * reset_to_98 -  takes a pointer to an int as parameter and
- * updates the value it points to 98.
+ * reset_to_98 - takes a pointer to an int as parameter and
+ * updates the value it points to to 98
  *
- * @n: pointer to update the value.
+ * @n: pointer to be updated.
  *
  */
 void reset_to_98(int *n)
-{
+{	
 	*n = 98;
 }
--- pointers_arrays_strings/3-puts.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-/**
- * _puts - prints a string, followed by a new line.
- * @str: string to be printed.
- *
- */
-void _puts(char *str)
-{	
-
