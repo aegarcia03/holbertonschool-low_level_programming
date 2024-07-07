f5a68af6115e550f9fa05637869deba2a3a12ec0
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-07-07 04:19:38 +0000

    reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev -  prints a string, in reverse, followed by a new line.
+ * @s: string to be printed.
+ *
+ * Return: string in reverse.
+ *
+ */
+void print_rev(char *s)
+{
+	int index;
+	int length = 0;
+
+	for (index = 0 ; s[index] != '\0'; index++)
+	{
+		length++;
+	}
+
+	for (index = length - 1; index >= 0 ; index--)
+	{
+		_putchar(s[index]);
+	}
+	_putchar('\n');
+}
