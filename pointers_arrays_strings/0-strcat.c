3b3c3e4e2153904a002876a856a9cf7fd4ef8ae2
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-07-08 02:04:35 +0000

    _strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,19 +11,19 @@ char *_strcat(char *dest, char *src)
 {
 	char *temp = dest;
 
-	while (dest != '\0')
+	while (*dest != '\0')
 	{
 		dest++;
 	}
 
-	while (src != '\0')
+	while (*src != '\0')
 	{
 		*dest = *src;
-		*dest++;
-		*src++;
+		dest++;
+		src++;
 	}
 
-	dest = '\0';
+	*dest = '\0';
 
 	return (temp);
 }
