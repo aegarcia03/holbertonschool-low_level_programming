4869c414f365748fdd45c2cbfb7b7449dd62dad9
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-07-09 00:10:45 +0000

    capitalizes all words of a string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -3,7 +3,7 @@
 /**
  * cap_string - capitalizes all words of a string.
  * @str: string to be checked.
- * 
+ *
  * Return: string with all its words capitalized.
  *
  */
@@ -12,5 +12,25 @@ char *cap_string(char *str)
 	int i;
 	char *ptr = str;
 
+	if (ptr[0] >= 'a' && ptr[0] <= 'z')
+	{
+		ptr[0] -= ('a' - 'A');
+	}
+
 	for (i = 0; ptr[i] != '\0'; i++)
 	{
+		if (ptr[i] == ' ' || ptr[i] == '\t' || ptr[i] == '\n' ||
+			ptr[i] == ',' || ptr[i] == ';' || ptr[i] == '.' ||
+			ptr[i] == '!' || ptr[i] == '?' || ptr[i] == '"' ||
+			ptr[i] == '(' || ptr[i] == ')' || ptr[i] == '{' ||
+			ptr[i] == '}')
+
+		{
+			if (ptr[i + 1] >= 'a' && ptr[i + 1] <= 'z')
+			{
+				ptr[i + 1] -= ('a' - 'A');
+			}
+		}
+	}
+	return (str);
+}
