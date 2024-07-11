a08a245b31d276f08f1028802a33f653cc27507e
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-07-11 08:42:34 +0000

    prints its name, followed by a new line.

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,3 +2,21 @@
 #include <stdio.h>
 
 /**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char* argv[])
+{
+	int i;
+
+	for (i = 0; argv[0][i] != '\0'; i++)
+	{
+		_putchar(argv[0][i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
