075b387fde01c031579a6a47ea946aefef0dafe4
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-07-12 00:18:24 +0000

    prime number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,21 +2,22 @@
 #include <stdio.h>
 
 /**
- * support_function: helps to check if n 
- * is divisible by any integer.
- * @i: square of n
- * @n: number to be checked.
+ * support_function - checks if a number is prime recursively.
+ * @i: the current divisor being checked.
+ * @n: number to check.
+ *
+ * Return: 1 if the number is prime, 0 otherwise.
  */
 int support_function(int n, int i)
-{	
+{
 	if (n <= 1)
-	{	
+	{
 		return (0);
 	}
 	if (i * i > n)
 	{
 		return (1);
-	}	
+	}
 	if (n % i == 0)
 	{
 		return (0);
@@ -24,9 +25,10 @@ int support_function(int n, int i)
 	return (support_function(n, i + 1));
 }
 /**
- * is_prime_number - returns 1 if the input integer is a prime number.
- * otherwise return 0.
- * @n: number to be checked.
+ * is_prime_number - checks if a number is a prime number.
+ * @n: number to check.
+ *
+ * Return: 1 if the number is prime, 0 otherwise.
  */
 int is_prime_number(int n)
 {
