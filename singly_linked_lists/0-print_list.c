1428d9c8c79ce7b2bc0420136e853d90a13ea1eb
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-07-30 04:06:35 +0000

    prints all the elements of a list_t list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,4 +12,21 @@
  * If str is NULL, print [0] (nil)
  */
 size_t print_list(const list_t *h)
-{}
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
