d743e6e2a53a71f7ed957031ea6e62b4668da045
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-07-30 02:28:53 +0000

    Working on the first file

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,3 +1,15 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: pointer to the first node of the linked list.
+ *
+ * Return: number of nodes
+ * If str is NULL, print [0] (nil)
+ */
+size_t print_list(const list_t *h)
+{}
