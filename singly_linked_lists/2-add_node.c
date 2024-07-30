1afae161ad5780977c28983cc7d6d7323e82f62f
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-07-30 04:44:15 +0000

    adds a new node at the beginning

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * add_node - adds a new node at the beginning of a list
+ * @head:
+ * @str:
+ *
+ * Return: the address of the new element
+ * NULL if it failed.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+
+}
