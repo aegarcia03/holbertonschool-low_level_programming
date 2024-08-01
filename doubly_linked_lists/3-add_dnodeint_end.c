1b60601df678ea58f98821caefd3626139d0091b
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-08-01 04:42:38 +0000

    adds a node at the end

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,7 +8,7 @@
  * @head: pointer to the head of the list
  * @n: point to the node to be added.
  *
- * Retur: the address of the new element
+ * Return: the address of the new element
  * or NULL if it failed.
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
