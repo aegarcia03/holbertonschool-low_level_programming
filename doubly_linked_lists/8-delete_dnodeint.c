e288ee6407b21ba7bc10806479de4787c2e7b287
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-08-02 06:28:11 +0000

    task 08

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 /**
  * delete_dnodeint_at_index - deletes the node at index
- * *head: pointer to the head of the list
+ * @head: pointer to the head of the list
  * @index: of the node that should be deleted.
  *
  * Return: 1 if success, -1 otherwise.
