b205436695ffc350680d450c2090ebc1f069c47d
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-08-02 06:27:11 +0000

    task 08

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -13,19 +13,18 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *tmp;
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (head != NULL || *head == NULL)
 		return (-1);
 
 	tmp = *head;
-
 	if (index == 0)
 	{
 		*head = tmp->next;
-		if (*head != NULL)
+		if (tmp->next != NULL)
 		{
-			(*head)->prev = NULL;
+			tmp->next->prev = NULL;
 		}
 		free(tmp);
 		return (1);
