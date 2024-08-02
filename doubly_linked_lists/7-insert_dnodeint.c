54af0c4d5b83cb7d10925e07e03ec0b7929c8cd0
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-08-02 01:58:46 +0000

    task 07

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -9,39 +9,45 @@
  * @idx: index of the list where node should be added
  * @n: the node to be inserted
  *
- * Return: the address of the new node 
+ * Return: the address of the new node
  * NULL if it fails
  * If is not possible, do not add and return NULL.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
-{	
-	dlistint_t *tmp;
-	delistint_t *new;
-	unsigned int i = 0; 
-	
-	new = malloc(sizeof(dlistint_t))
-	
+{
+	dlistint_t *tmp = *h;
+	dlistint_t *new;
+	unsigned int i = 0;
+
+	new = malloc(sizeof(dlistint_t));
+
 	if (new == NULL)
 		return (NULL);
-	
+
 	new->n = n;
 
 	if (idx == 0)
-		return (add-dnodeint(h, n));
-	
-	new->n = n; 
+		return (add_dnodeint(h, n));
 
-	add-dnodeint // adds a new node at the beginning
-	add_dnodeint_end // adds a node at the end 
-
-	dlistint_t prev = h;
-	for (i = 0; i < idx - 1; i++)
+	for (i = 0; tmp != NULL && i < idx - 1; i++)
 	{
-		if (prev == NULL)
-		{
-			return (h);
-		}
-		prev = prev->next;
+		tmp = tmp->next;
 	}
 
+	if (tmp == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new->next = tmp->next;
+	new->prev = tmp;
+	if (tmp->next != NULL)
+		tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
 }
