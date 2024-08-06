9d2c51364d7509131815ead8a42f4b8758c2648c
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-08-06 01:20:47 +0000

    task 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/**
+ * main -  copies the content of a file to another file.
+ * @argc: the numnber of arguments.
+ * @argv: the arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+
+return (0);
+}
