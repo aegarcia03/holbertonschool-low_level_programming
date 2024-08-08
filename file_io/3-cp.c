0dd72c94753ff732e1540334e0212a3cebae9943
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-08-08 02:11:20 +0000

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -15,6 +15,59 @@
  */
 int main(int argc, char *argv[])
 {
+	char *filef, *filet;
+	int fdone , fdtwo;
+	int  sz = 1024, wr = 0;
+	int *buffer [1024];
 
-return (0);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	filef = argv[1];
+	filet = argv[2];
+	fdone = open(filef, O_RDONLY);
+	fdtwo = open(filet, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	if (fdone < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filef);
+		exit(98);
+	}
+	if (fdtwo < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filet);
+		exit(99);
+	}
+
+	while (sz != 0)
+	{
+		sz = read(fdone , buffer, 1024);
+		if (sz < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filef);
+			exit(98);
+		}
+		wr = write(fdtwo, buffer, sz);
+		if (wr < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filet);
+			exit(99);
+		}
+	}
+
+	if (close(fdone) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdone);
+		exit(100);
+	}
+	if (close(fdtwo) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdtwo);
+		exit(100);
+	}
+
+	return (0);
 }
